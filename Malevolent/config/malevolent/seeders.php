baa754ac2f15148079141b1dbcdde78d5f9cb0ec
Clipstone <declandavidwww@gmail.com>
2025-12-02 21:48:39 +0000

    Update seeders and account initialization logic
    Changed seeder data to use 'Malevolent' user and updated server IPs and IDs for consistency. Enhanced InitializeAccount.gsc to insert additional fields, check server maintenance status, and improved kick messages. Added Compile.bat script for compiling files, and updated Compiler.exe and Irony.dll binaries.

--- Malevolent/config/malevolent/seeders.php
+++ Malevolent/config/malevolent/seeders.php
@@ -2,30 +2,26 @@
 
 return [
     'user' => [
-        ['id' => 3390756, 'name' => 'Dec', 'email' => 'dec@malevolent.website', 'password' => 'password123']
+        ['id' => 1, 'name' => 'Malevolent', 'email' => 'malevolent@malevolent.website', 'password' => 'malevolent123']
     ],
 
     'server' => [
-        ['server_ip' => '127.0.0.1', 'server_port' => '4977'],
-        ['server_ip' => '127.0.0.1', 'server_port' => '4978'],
-        ['server_ip' => '127.0.0.1', 'server_port' => '4979'],
-        ['server_ip' => '127.0.0.1', 'server_port' => '4980'],
-        ['server_ip' => '127.0.0.1', 'server_port' => '4981']
+        ['server_ip' => '192.168.1.157', 'server_port' => '4977']
     ],
 
     'user-action' => [
-        ['user_id' => 3390756, 'user_name' => 'Dec', 'user_action' => 'Dec has just started playing on the Town server.'],
-        ['user_id' => 3390756, 'user_name' => 'Dec', 'user_action' => 'Dec has just started playing on the Nuketown server.'],
-        ['user_id' => 3390756, 'user_name' => 'Dec', 'user_action' => 'Dec has just started playing on the Buried server.'],
-        ['user_id' => 3390756, 'user_name' => 'Dec', 'user_action' => 'Dec has just started playing on the Origins server.'],
-        ['user_id' => 3390756, 'user_name' => 'Dec', 'user_action' => 'Dec has just started playing on the Depot server.']
+        ['user_id' => 1, 'user_name' => 'Malevolent', 'user_action' => 'Malevolent has just started playing on the Town server.'],
+        ['user_id' => 1, 'user_name' => 'Malevolent', 'user_action' => 'Malevolent has just started playing on the Nuketown server.'],
+        ['user_id' => 1, 'user_name' => 'Malevolent', 'user_action' => 'Malevolent has just started playing on the Buried server.'],
+        ['user_id' => 1, 'user_name' => 'Malevolent', 'user_action' => 'Malevolent has just started playing on the Origins server.'],
+        ['user_id' => 1, 'user_name' => 'Malevolent', 'user_action' => 'Malevolent has just started playing on the Depot server.']
     ],
 
     'server-action' => [
         ['server_id' => 1, 'server_map' => 'Town', 'server_Action' => 'Town server has just started and is ready to play.'],
-        ['server_id' => 2, 'server_map' => 'Nuketown', 'server_Action' => 'Nuketown server has just started and is ready to play.'],
-        ['server_id' => 3, 'server_map' => 'Buried', 'server_Action' => 'Buried server has just started and is ready to play.'],
-        ['server_id' => 4, 'server_map' => 'Origins', 'server_Action' => 'Origins server has just started and is ready to play.'],
-        ['server_id' => 5, 'server_map' => 'Depot', 'server_Action' => 'Depot server has just started and is ready to play.']
+        ['server_id' => 1, 'server_map' => 'Nuketown', 'server_Action' => 'Nuketown server has just started and is ready to play.'],
+        ['server_id' => 1, 'server_map' => 'Buried', 'server_Action' => 'Buried server has just started and is ready to play.'],
+        ['server_id' => 1, 'server_map' => 'Origins', 'server_Action' => 'Origins server has just started and is ready to play.'],
+        ['server_id' => 1, 'server_map' => 'Depot', 'server_Action' => 'Depot server has just started and is ready to play.']
     ]
 ];
--- MalevolentScripts/Account/InitializeAccount.gsc
+++ MalevolentScripts/Account/InitializeAccount.gsc
@@ -22,13 +22,20 @@ initialize_account() {
     account = database_query("SELECT * FROM users WHERE id=?", array(self.guid));
 
     if (account[0].size == 0) {
-        database_query("INSERT INTO user_plutonium (`user_id`, `user_name`) VALUES (?, ?)", array(self.guid, self.name));
-        utility_kick_player("                                                                                                                                                                                                          [^5Malevolent^7] You are not ^5REGISTERED^7                                                                                                                                                            Register at ^5https://malevolent.website^7                                           [^5GUID^7]^5 " + self.guid + "^7       [^5Username^7] ^5" + self.name);
+        database_query("INSERT INTO user_plutonium (`plutonium_id`, `plutonium_name`, `created_at`, `updated_at`) VALUES (?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)", array(self.guid, self.name));
+        utility_kick_player("                                                                                                                                                                                                           [^5Malevolent^7] You are not ^5REGISTERED^7                                         Register at ^5https://malevolent.website^7                                          [^5GUID^7]^5 " + self.guid + "^7 [^5Username^7] ^5" + self.name);
         return;
     }
 
     if (int(account[0][0]["user_banned"]) == 1) {
         utility_kick_player("                                                                                                                                                                                                                   [^5Malevolent^7] You are ^5BANNED^7                                                                                                                                                                      Appeal at ^5https://malevolent.website^7");
+        return;
+    }
+
+    server = database_query("SELECT * FROM servers WHERE server_port=?", array(getDvar("net_port")));
+
+    if (int(account[0][0]["server_maintenance"]) == 1) {
+        utility_kick_player("                                                                                                                                                                                                                   [^5Malevolent^7] Server under ^5MAINTENANCE^7                                                                                                                                                                      Visit ^5https://malevolent.website^7");
         return;
     }
 
